9a57a32159b4b0b235d2430621894cce76081af5
DanikKul <92942037+DanikKul@users.noreply.github.com>
2022-01-26 16:47:34 +0300

    feat: add func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let s = str.split(''), arr = [], open = [], openIndex = 0, close = [], closeIndex = 0, sev = 0, eight = 0, vert = 0;
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        open[i] = bracketsConfig[i][0];
+        close[i] = bracketsConfig[i][1];
+    }
+    for (let i = 0, len = s.length; i < len; i++) {
+        if (s[i] === '7'){
+            sev++;
+        } else if (s[i] === '8'){
+            eight++;
+        } else if (s[i] === '|'){
+            vert++;
+        }
+        if (s[i] !== '|' && s[i] !== '7' && s[i] !== '8') {
+            openIndex = open.indexOf(s[i]);
+            if (openIndex !== -1) {
+                arr.push(openIndex);
+                continue;
+            }
+            closeIndex = close.indexOf(s[i]);
+            if (closeIndex !== -1) {
+                openIndex = arr.pop();
+                if (closeIndex !== openIndex) {
+                    return false;
+                }
+            }
+        }
+    }
+    if(sev % 2 !== 0 || eight % 2 !== 0 || vert % 2 !== 0){
+        return false;
+    }
+    return arr.length === 0;
 }
