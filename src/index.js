395745d2e0160acd24da086a9251abfad6f2dfd3
DanikKul <92942037+DanikKul@users.noreply.github.com>
2022-01-28 16:20:02 +0300

    fix: fix func

--- src/index.js
+++ src/index.js
@@ -1,34 +1,44 @@
 module.exports = function check(str, bracketsConfig) {
-    let s = str.split(''), arr = [], open = [], openIndex = 0, close = [], closeIndex = 0, sev = 0, eight = 0, vert = 0;
-    for (let i = 0; i < bracketsConfig.length; i++) {
-        open[i] = bracketsConfig[i][0];
-        close[i] = bracketsConfig[i][1];
+  let sum = 0;
+  let up = [];
+  let down = [];
+  let squance = [];
+  let oper = false;
+  for (type of bracketsConfig){
+    up.push(type[0]);
+    down.push(type[1]);
+  };
+
+  for (let i = 0; i < str.length; i++){
+    oper = false;
+    if (up.indexOf(str[i]) !== -1) {
+      if (up[up.indexOf(str[i])] === down[up.indexOf(str[i])]){
+        if (squance[squance.length - 1] === str[i]){
+        oper = false;
+        }else{
+        oper = true;
+        squance.push(down[up.indexOf(str[i])]);
+        sum++;   
+        };
+      } else{
+      squance.push(down[up.indexOf(str[i])]);
+      sum++;
+      }
     }
-    for (let i = 0, len = s.length; i < len; i++) {
-        if (s[i] === '7'){
-            sev++;
-        } else if (s[i] === '8'){
-            eight++;
-        } else if (s[i] === '|'){
-            vert++;
-        }
-        if (s[i] !== '|' && s[i] !== '7' && s[i] !== '8') {
-            openIndex = open.indexOf(s[i]);
-            if (openIndex !== -1) {
-                arr.push(openIndex);
-                continue;
-            }
-            closeIndex = close.indexOf(s[i]);
-            if (closeIndex !== -1) {
-                openIndex = arr.pop();
-                if (closeIndex !== openIndex) {
-                    return false;
-                }
-            }
-        }
-    }
-    if(sev % 2 !== 0 || eight % 2 !== 0 || vert % 2 !== 0){
+    if (down.indexOf(str[i]) !== -1 && !oper){
+      sum--;
+      if (sum < 0) {
         return false;
+      }
+      if (squance[squance.length - 1] === str[i]) {
+        squance.splice(squance.length - 1, 1);
+      }else{
+        return false;
+      }
+
     }
-    return arr.length === 0;
+    
+    
+  }
+  return (sum === 0)? true: false;
 }
